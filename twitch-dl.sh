e2a45c190833e5a2f87c33ead869560b6ed70fd4
Jamie <j@mieis.sexy>
2021-07-08 03:36:40 -0400

    typechange: twitch-dl.sh

--- twitch-dl.sh
+++ twitch-dl.sh
@@ -1,1 +1,11 @@
-/usr/local/bin/twitch-dl
+#!/bin/bash
+cd ~/
+URL=`xclip -o -selection clipboard`
+twid=`sed 's/[^0-9]//g' <<<$URL`
+twitchfn=`youtube-dl --get-filename -o "%(uploader)s - %(upload_date)s - %(title)s" $URL`
+youtube-dl $URL &  PIDVD=$! 
+tcd --video $twid --format ssa --output Movies/ &  PIDCH=$!
+wait $PIDVD
+wait $PIDCH
+ffmpeg -i "Movies/${twitchfn}.mp4" -i "Movies/${twid}.ssa" -c:v copy -c:a copy -c:s copy "Movies/${twitchfn}.mkv"
+rm "Movies/${twid}.ssa" | rm "Movies/${twitchfn}.mp4"
